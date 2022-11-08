07e796fa30fca556a5a599d6e4df47ae9319ba9e
AsteXOriginal <117456510+AsteXOriginal@users.noreply.github.com>
2022-11-08 16:48:32 +0200

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,20 @@
+d = {1: 'one',2: 'two',3: 'three',4: 'four',}
+print(d)
+#len - підрахунок
+print(len(d))
+#dict.get(key, [value])
+print(d.get(2))
+#dict.setdefault(key, [value])
+print(d.setdefault(4))
+#dict.pop(key)
+print(d.pop(3))
+#dict.keys()
+print(d.keys())
+#dict.values
+print(d.keys())
+#dict.items
+print(d.items())
+#dict.clear()
+d.clear()
+print(d)
+input()
---
+++ форма реестрації за допомогою циклів.py
@@ -0,0 +1,5 @@
+form = {'Username' : '', 'Password' : ''}
+form['Username'] = input('Input username: ')
+form['Password'] = input('Input password: ')
+print('User' + form['Username'] + 'logged with password' + form['Password'])
+input()
